9cf4e7a13ebf54476f57ab834223f412b9d85a06
Dom <55361342+sneakerheadz1@users.noreply.github.com>
2020-08-19 21:01:56 -0700

    Add files via upload

---
+++ Dom's_BACKUP.ps1
@@ -0,0 +1,11 @@
+﻿# Backup Script for Inlander DATA Using Robocopy to Remote Destination
+
+
+#Set-ExecutionPolicy Unrestricted
+
+$Inlander_backupsc = 'C:\Users\User\Documents\Inlander_source' # Inlander local data files
+$Inlander_destination = 'C:\Users\IEUser\Desktop\Share'        #Inlander remote data files
+$Inlander_log = 'C:\Users\User\Documents\Inlander_log.txt'   #Inlander data analysis log remote
+
+ROBOCOPY $Inlander_backupsc $Inlander_destination /Copyall /E /ZB /DCOPY:DAT /R:3 /W:15 /LOG:$Inlander_log
+
